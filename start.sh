1a4adb1dd2aa5cfe677d8c9f6d0894228bcce31f
s-odhiambo <54894835+s-odhiambo@users.noreply.github.com>
2019-12-04 13:09:07 +0300

    delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-export SECRET_KEY='samm'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:12345\@localhost/blog_up'
-export MAIL_USERNAME='samuelangienda1998@gmail.com'
-export MAIL_PASSWORD='12345'
-export DATABASE_URL='postgresql+psycopg2://moringa:12345@localhost/blog_up'
-
-python3.6 manage.py server
